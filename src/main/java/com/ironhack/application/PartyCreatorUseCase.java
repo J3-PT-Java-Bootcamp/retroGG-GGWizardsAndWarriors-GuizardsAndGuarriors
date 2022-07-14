14d8776ccc441cd14d607ef026016037c10dc070
Ana Montero <anamongar@gmail.com>
2022-07-15 00:57:09 +0200

    Create random players on a party with size chosen by player

--- src/main/java/com/ironhack/application/PartyCreatorUseCase.java
+++ src/main/java/com/ironhack/application/PartyCreatorUseCase.java
@@ -1,16 +1,11 @@
 package com.ironhack.application;
 
-import com.ironhack.domain.characters.warrior.Warrior;
-import com.ironhack.domain.characters.warrior.stats.WarriorBaseStats;
 import com.ironhack.domain.characters.warrior.stats.attributes.Stamina;
-import com.ironhack.domain.characters.wizard.Wizard;
-import com.ironhack.domain.characters.wizard.stats.WizardBaseStats;
 import com.ironhack.domain.exceptions.IllegalCharacterClassException;
 import com.ironhack.domain.party.Party;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class PartyCreatorUseCase {
 
@@ -34,17 +29,12 @@ public class PartyCreatorUseCase {
                 switch (option) {
                     case 1 -> {
                         System.out.println("Create new character");
-                        createCharacter();
                         // create party usecase
                         //generate random enemy party
                         //battle
                     }
                     case 2 -> {
-                        if(!party.isEmpty()){
-                            System.out.println(party);
-                        } else{
-                            System.out.println("Party is empty");
-                        }
+                        System.out.println(party);
                     }
                     case 9 -> {
                         exit = true;
@@ -54,8 +44,6 @@ public class PartyCreatorUseCase {
             } catch (InputMismatchException exception) {
                 System.out.println("The selected option is not valid. Please, select a valid option:");
                 this.scanner.next();
-            } catch (IllegalCharacterClassException e) {
-                System.out.println("The selected class is not valid. Start over the character creation. ");
             }
         }
         return party;
@@ -69,15 +57,8 @@ public class PartyCreatorUseCase {
         if(characterClass.toLowerCase().trim().equals("warrior")) {
             System.out.println(String.format("How much stamina? (%d-%d): ", Stamina.MIN_VALUE, Stamina.MAX_VALUE));
             var stamina = this.scanner.nextInt();
-//            System.out.println(String.format("How much stamina? (%d-%d): ", MIN_VALUE, Stamina.MAX_VALUE));
-//            var strength = this.scanner.nextInt();
-//            WarriorBaseStats = new WarriorBaseStats(stamina,)
-//            if (stamina < Stamina.MIN_VALUE && stamina > Stamina.MAX_VALUE){
-//                Warrior.create(UUID.randomUUID(),name,);
-//            }
 
         } else if (characterClass.toLowerCase().trim().equals("wizard")) {
-//            Wizard.create(UUID.randomUUID(), stats,);
 
         } else {
             throw new IllegalCharacterClassException();
--- src/main/java/com/ironhack/application/PartyRandomCreator.java
+++ src/main/java/com/ironhack/application/PartyRandomCreator.java
@@ -2,6 +2,7 @@ package com.ironhack.application;
 
 import com.ironhack.domain.characters.warrior.stats.attributes.Stamina;
 import com.ironhack.domain.exceptions.IllegalCharacterClassException;
+import com.ironhack.domain.generators.Generator;
 import com.ironhack.domain.party.Party;
 
 import java.util.InputMismatchException;
@@ -10,13 +11,13 @@ import java.util.Scanner;
 public class PartyRandomCreator {
 
     private final Scanner scanner;
+    Party party = new Party();
 
     public PartyRandomCreator() {
         this.scanner = new Scanner(System.in);
     }
 
     public Party run() {
-        var party = new Party();
 
         boolean exit = false;
         int option;
@@ -28,8 +29,10 @@ public class PartyRandomCreator {
                 option = this.scanner.nextInt();
                 switch (option) {
                     case 1 -> {
-                        System.out.println("Create new party");
-                        // create party usecase
+                        System.out.println("Create new random party");
+                        System.out.println("Party size: ");
+                        int size = this.scanner.nextInt();
+                        createParty(size);
                         //generate random enemy party
                         //battle
                     }
@@ -53,9 +56,15 @@ public class PartyRandomCreator {
         return party;
     }
 
-    private void createParty() {
-        System.out.println("Party size: ");
-        var name = this.scanner.nextInt();
+    private void createParty(int size) {
+        for (int i = 0; i < size; i++) {
+            if (this.getRandomInt() == 1) {
+                party.addMember(Generator.randomWizard());
+
+            } else {
+                party.addMember(Generator.randomWarrior());
+            }
+        }
     }
 
     private void printOptions() {
@@ -64,5 +73,9 @@ public class PartyRandomCreator {
         System.out.println("\t2 - View the current party characters");
         System.out.println("\t9 - Finish party creation");
         System.out.println("Enter an option:");
+    }
+
+    private int getRandomInt() {
+        return (int) (Math.random() * (2 + 1));
     }
 }
